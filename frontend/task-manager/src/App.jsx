4ff1b9da8d1d1e58d4b1830d05b88a00876763b5
kunstewi <kan.ai35423@gmail.com>
2025-06-12 23:58:54 +0530

    routers admin

--- frontend/task-manager/src/App.jsx
+++ frontend/task-manager/src/App.jsx
@@ -13,6 +13,23 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signUp" element={<SignUp />} />
+
+          {/* Admin Routes */}
+          <Route element={<privateRoute allowdRoles={["admin"]} />}>
+
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/tasks" element={<ManageTasks />} />
+          <Route path="/admin/create-task" element={<CreateTask />} />
+          <Route path="/admin/users" element={<ManageUsers />} />
+
+          {/* Users Routes */}
+          <Route>
+            
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+ 
+          </Route>
+            
+          </Route>
         </Routes>
       </Router>
     </div>
